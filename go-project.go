2104eb2b43d3cecd9252264e7597e3269d7d72fd
Andi Zhou <andiz.aus@gmail.com>
2021-11-27 17:38:59 +1100

    Scaffolded app

--- go-project.go
+++ go-project.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type Report struct {
+	Timestamp int64
+}
+
+func retrieveData(path string) Report {
+	if path == "" {
+		path = "./input.json"
+	}
+	return Report{}
+}
+
+func validateData(data Report) bool {
+	return false
+}
+func logData(data Report) {
+}
+func postData(data Report) string {
+	return ""
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var data Report = retrieveData("")
+	var valid bool = validateData(data)
+	if !valid {
+		fmt.Println("Input JSON was not valid")
+		os.Exit(1)
+	}
+	logData(data)
+	postData(data)
 }
